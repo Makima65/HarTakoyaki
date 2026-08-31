46a13ad33dcc3e197e354967c63052c91ef55241
Makima65 <ralphchryslersilva65@gmail.com>
2026-09-01 05:50:11 +0800

    remove extrta section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,6 @@ import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import About from "@/components/About";
 import Menu from "@/components/Menu";
-import HowItWorks from "@/components/HowItWorks";
-import Amenities from "@/components/Amenities";
 import Gallery from "@/components/Gallery";
 import Reviews from "@/components/Reviews";
 import Inquire from "@/components/Inquire";
@@ -17,8 +15,6 @@ export default function Home() {
       <Hero />
       <About />
       <Menu />
-      <HowItWorks />
-      <Amenities />
       <Gallery />
       <Reviews />
       <Inquire />
